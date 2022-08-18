f998dc7087271cb681c01acfc17fb9655c388669
Eightyeightdays <eightyeightdays@gmail.com>
2022-08-18 18:38:43 +0200

    authorisation added for simple GET user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
-router.get("/users", userController.getAll);        
-router.get("/users/:id", userController.getById);
-router.get("/users/:id/posts", userController.getPosts);
+router.get("/users", auth, userController.getAll);        
+router.get("/users/:id", auth, userController.getById);
+router.get("/users/:id/posts", auth, userController.getPosts);
 router.post("/auth/signup", userController.createUser);
 router.post("/auth/login", userController.loginUser);
 router.post("/auth/logout", userController.logoutUser);
